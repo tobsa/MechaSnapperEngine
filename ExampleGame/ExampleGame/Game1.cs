8d05356930534499374d2c5ec4b87c396283dcb0
Tacohej <mctinn@gmail.com>
2014-12-19 15:28:10 +0100

    Fixed

--- ExampleGame/ExampleGame/Game1.cs
+++ ExampleGame/ExampleGame/Game1.cs
@@ -47,17 +47,6 @@ namespace ExampleGame
         /// </summary>
         protected override void LoadContent()
         {
-            Entity background = EntityFactory.CreateEntity(EntityFactory.GenerateID, Content.Load<Texture2D>("Sky"), new Vector2(0,0));
-            Entity barrarok = EntityFactory.CreateEmptyEntity(EntityFactory.GenerateID, new Vector2(8 * 64, 10 * 64 + 8));
-            Entity jack = EntityFactory.CreateEntity(EntityFactory.GenerateID, Content.Load<Texture2D>("UnluckyJack126"), new Vector2(2 * 64, 6 * 64));
-            //AgentComponent agent = new AgentComponent();
-            //agent.Behaviour = new SimpleAI();
-            //ComponentManager.Instance.AddComponent<InputComponent>(entity1, new InputComponent());
-            //ComponentManager.Instance.AddComponent<VelocityComponent>(entity1, new VelocityComponent() { Velocity = new Vector2(50, -200) });
-            //ComponentManager.Instance.AddComponent<RigidBodyComponent>(entity1, new RigidBodyComponent() { Friction = 0.1f, Gravity = 32});
-            //ComponentManager.Instance.AddComponent<AgentComponent>(entity3, agent);
-            //ComponentManager.Instance.AddComponent<AnimationComponent>(entity5, barrarokAnim);
-            //ComponentManager.Instance.AddComponent<RenderComponent>(entity5, new RenderComponent(Content.Load<Texture2D>("BarrarokAnim"), 64, 124, 0));
             int[,] rocks = new int[,] 
             {
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -75,14 +64,9 @@ namespace ExampleGame
             {0, 0, 8, 8, 8, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0}, 
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
             };
+
             List<Entity> rockEntities = EntityFactory.CreateTileWorld(rocks, Content.Load<Texture2D>("Rocks_FG_64x64"), 64, 64);
-            //engine.SceneManager.AddEntity("World1.Level1.Room1", 0, entity3);
-            //engine.SceneManager.AddEntity("World1.Level1.Room1", -1, entity4);
-            //engine.SceneManager.AddEntity("World1.Level1.Room1", 0, entity5);
-            //engine.SceneManager.AddEntity("World1.Level2.Room1", 1, entity1);
-            //engine.SceneManager.AddEntity("World1.Level2.Room1", 0, entity2);
-            engine.SceneManager.SetCurrentScene("World1.Level1.Room1");
-=======
+
             Entity background = EntityFactory.CreateEntity(EntityFactory.GenerateID, Content.Load<Texture2D>("Sky"), new Vector2(0,0));
             Entity barrarok = EntityFactory.CreateEmptyEntity(EntityFactory.GenerateID, new Vector2(8 * 64, 10 * 64 + 8));
             Entity jack = EntityFactory.CreateEntity(EntityFactory.GenerateID, Content.Load<Texture2D>("UnluckyJack126"), new Vector2(2 * 64, 6 * 64));
@@ -95,15 +79,13 @@ namespace ExampleGame
             ComponentManager.Instance.AddComponent<RigidBodyComponent>(jack, new RigidBodyComponent(28f, 0.3f, 0f));
             ComponentManager.Instance.AddComponent<CollisionRectangleComponent>(jack, new CollisionRectangleComponent(new Rectangle(2 * 64, 6 * 64, 128, 128)));
 
-            var tiles = TileManager.LoadLevel(Content.Load<Texture2D>("Box64"));
-            foreach (var tile in tiles)
-                engine.SceneManager.AddEntity("Level1", 0, tile);
-
+            
             engine.SceneManager.AddEntity("Level1", 0, background);
             engine.SceneManager.AddEntity("Level1", 1, barrarok);
             engine.SceneManager.AddEntity("Level1", 2, jack);
+            engine.SceneManager.AddEntities("Level1", 1, rockEntities);
             engine.SceneManager.SetCurrentScene("Level1");
->>>>>>> 22f87a0d47315d861a38ea2c066f15186bb19887
+
 
             InputManager.Instance.AddKeyBinding("Exit", Keys.Escape);
             InputManager.Instance.AddKeyBinding("Left", Keys.Left);
@@ -120,17 +102,10 @@ namespace ExampleGame
 
             var playingState = new PlayingState(engine);
             playingState.RegisterSystem(new RenderSystem(engine.SceneManager, engine.SpriteBatch));
-<<<<<<< HEAD
-            //playingState.RegisterSystem(new InputSystem(engine.SceneManager));
-            //playingState.RegisterSystem(new AISystem(engine.SceneManager));
-            //playingState.RegisterSystem(new PhysicsSystem(engine.SceneManager));
-            //playingState.RegisterSystem(new AnimationSystem(engine.SceneManager, engine.SpriteBatch));
-=======
             playingState.RegisterSystem(new InputSystem(engine.SceneManager));
-            //playingState.RegisterSystem(new AISystem(engine.SceneManager));
             playingState.RegisterSystem(new PhysicsSystem(engine.SceneManager));
             playingState.RegisterSystem(new AnimationSystem(engine.SceneManager, engine.SpriteBatch));
->>>>>>> 22f87a0d47315d861a38ea2c066f15186bb19887
+
 
             engine.RegisterState(playingState);
             engine.RegisterState(new MainMenuState(engine));
