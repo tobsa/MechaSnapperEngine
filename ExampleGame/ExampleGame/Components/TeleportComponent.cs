180e1379fefd03e634da17997bce0d0cf0ff8623
S123013 <s123013@student.hb.se>
2014-12-25 16:54:34 +0100

    Begining of teleport component

---
+++ ExampleGame/ExampleGame/Components/TeleportComponent.cs
@@ -0,0 +1,19 @@
+﻿using GameEngine.Components;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExampleGame.Components {
+    class TeleportComponent : IComponent{
+        public static const int InChamber = 0;
+        public static const int OnAir = 1;
+        public static const int OnGround = 2;
+        public int State { get; set; }
+        public float ElevationDegree { get; set;}
+        public float Velocity { get; set; }
+        public float VelocityX { get; set; }
+        public float VelocityY { get; set; }
+    }
+}
