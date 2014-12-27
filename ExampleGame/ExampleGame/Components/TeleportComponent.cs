dcf8b740c05d263a02e471f5dcca6cd8f1778142
S123013 <s123013@student.hb.se>
2014-12-28 00:36:53 +0100

    still testing

--- ExampleGame/ExampleGame/Components/TeleportComponent.cs
+++ ExampleGame/ExampleGame/Components/TeleportComponent.cs
@@ -7,11 +7,12 @@ using System.Text;
 
 namespace ExampleGame.Components {
     class TeleportComponent : IComponent{
-        public static const int InChamber = 0;
-        public static const int OnAir = 1;
-        public static const int OnGround = 2;
+        public const int InChamber = 0;
+        public const int JustFired = 1;
+        public const int OnAir = 2;
+        public const int OnGround = 3;
         public int State { get; set; }
-        public float ElevationDegree { get; set;}
+        public float ElevationDegreePercent { get; set;}
         public float Velocity { get; set; }
         public float VelocityX { get; set; }
         public float VelocityY { get; set; }
